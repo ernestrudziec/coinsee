6c4f96ea7e37d936ee4ca564c28bbbf6d4f06ebc
Ernest Rudziec <ernest.rudziec@triggle.app>
2023-11-15 23:22:51 +0100

    fix: env variables

--- src/database/firebase/setup.ts
+++ src/database/firebase/setup.ts
@@ -1,18 +1,17 @@
 import { FirebaseOptions, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 import "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyAoBWYCmyHgy89KhEC7SqROaRN629g01r8",
-  authDomain: "coinsee-72018.firebaseapp.com",
-  projectId: "coinsee-72018",
-  storageBucket: "coinsee-72018.appspot.com",
-  messagingSenderId: "820511466875",
-  appId: "1:820511466875:web:90af44e4c9fb6271fe49de",
-  measurementId: "G-GHZG97K1YL",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // initialize
